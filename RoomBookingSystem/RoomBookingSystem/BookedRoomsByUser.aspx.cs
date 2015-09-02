a52bec3edafc2b91346d662450b5f9144d110fd6
John Bryan Basa <john.basa@robertsoncollege.net>
2015-09-02 17:05:45 -0500

    code behind for BookedRoomsByUser.aspx

---
+++ RoomBookingSystem/RoomBookingSystem/BookedRoomsByUser.aspx.cs
@@ -0,0 +1,33 @@
+﻿using DAL_Project;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RoomBookingSystem
+{
+    public partial class BookedRoomsByUser : System.Web.UI.Page
+    {
+        string conn = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+        Security mySecurity = new Security();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loadgvUserBookedRooms();   
+            }
+        }
+
+        public void loadgvUserBookedRooms()
+        {
+            DAL d = new DAL(conn);
+            d.AddParam("@UserID", mySecurity.UserID);
+            gvUserBookedRooms.DataSource = d.ExecuteProcedure("spGetBookingsForUser");
+            gvUserBookedRooms.DataBind();
+        }
+    }
+}
